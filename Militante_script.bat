14628aa3f071a30de6d62a28faa3e6ce44c2fbe3
nyxtante <422001988@ntc.edu.ph>
2025-07-04 22:15:54 +0800

    Add Militante_script.bat

---
+++ Militante_script.bat
@@ -0,0 +1,13 @@
+@echo off
+cls
+echo ================================
+echo      SYSTEM IP INFORMATION
+echo ================================
+ipconfig
+echo.
+echo Saving IP configuration to ipconfig_output.txt...
+ipconfig > ipconfig_output.txt
+echo Done!
+echo Cargel Jeunyx P. Militante
+echo BSIT 3.4
+pause
